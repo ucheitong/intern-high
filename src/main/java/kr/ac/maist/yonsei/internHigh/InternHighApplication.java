1f6bacce411d3932b8360a6525dc4c19c7c1e775
minwoo <ucheitong2@naver.com>
2021-09-06 15:19:45 +0900

    Initial commit

---
+++ src/main/java/kr/ac/maist/yonsei/internHigh/InternHighApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.maist.yonsei.internHigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternHighApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternHighApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/maist/yonsei/internHigh/InternHighApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.maist.yonsei.internHigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternHighApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
